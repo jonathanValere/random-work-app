0360186c777d0a1b0bce6f3309ffc7fdaebc6add
“Jonathan <jonathan.valere@gmail.com>
2024-05-15 23:09:22 -0300

    footer position fixed bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-slate-950 text-gray-400 text-center p-5 absolute bottom-0 w-full font-thin font-sans text-sm">
+    <footer className="bg-slate-950 text-gray-400 text-center p-5 fixed bottom-0 w-full font-thin font-sans text-sm">
       © 2024 | Culture et Compétences Numériques | Jonathan VALERE
     </footer>
   );
